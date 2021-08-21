cc58fb093d09865caac00541aacff6087675b475
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-08-21 02:36:35 -0400

    Initial commit

---
+++ project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUI.lua
@@ -0,0 +1,14 @@
+require "Items/SuburbsDistributions"
+require "Items/ItemPicker"
+
+    table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, "Base.Hat_CavalryHat");
+    table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, 5);
+    table.insert(SuburbsDistributions["gunstore"]["locker"].items, "Base.Hat_CavalryHat");
+    table.insert(SuburbsDistributions["gunstore"]["locker"].items, 1.3);
+
+    table.insert(SuburbsDistributions["armysurplus"]["metal_shelves"].items, "Base.Hat_CavalryHat");
+    table.insert(SuburbsDistributions["armysurplus"]["metal_shelves"].items, 3);
+    table.insert(SuburbsDistributions["armystorage"]["metal_shelves"].items, "Base.Hat_CavalryHat");
+    table.insert(SuburbsDistributions["armystorage"]["metal_shelves"].items, 3);
+
+
