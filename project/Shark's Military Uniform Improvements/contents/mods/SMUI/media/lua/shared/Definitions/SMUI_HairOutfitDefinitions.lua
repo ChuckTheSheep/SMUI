8a2ed1611727890a73927a9324d98d87c598bc98
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-08-26 00:41:40 -0400

    Create SMUI_HairOutfitDefinitions.lua

---
+++ project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/Definitions/SMUI_HairOutfitDefinitions.lua
@@ -0,0 +1,63 @@
+require 'Definitions/HairOutfitDefinitions'
+
+
+--Shark's Soldier Uniform Improvements Additions--
+
+local cat = {};
+cat.outfit = "SharkArmyCamoGreenMale";
+cat.haircut = "Short:100";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyCamoGreenFemale";
+cat.haircut = "Fresh:30;Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyCamoDesertMale";
+cat.haircut = "Short:50;CrewCut:20;Fresh:20;Bald:10";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyCamoDesertFemale";
+cat.haircut = "Hat:30;Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyInstructor";
+cat.haircut = "Short:50;CrewCut:20;Fresh:30";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyCamoUrbanMale";
+cat.haircut = "Fresh:100";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyCamoUrbanFemale";
+cat.haircut = "Hat:30;Bun:20;Kate:20;Ponytail:30";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "SharkArmyCamoUrbanGeared";
+cat.haircut = "Short:50;CrewCut:20;Fresh:20;Bald:10";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+-- Expanded Helicopter Events --
+
+local cat = {};
+cat.outfit = "1Soldier";
+cat.haircut = "Fresh:20;Bun:20;Kate:20;Ponytail:20";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
+local cat = {};
+cat.outfit = "1Soldier";
+cat.haircut = "Short:30;CrewCut:50;Fresh:20";
+cat.beard = "None:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
+
