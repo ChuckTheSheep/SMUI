50134f24e0fc794d4236c1f7610d2349da8a49bc
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-08-30 00:34:45 -0400

    Update SMUI_HairOutfitDefinitions.lua

--- project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/Definitions/SMUI_HairOutfitDefinitions.lua
+++ project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/Definitions/SMUI_HairOutfitDefinitions.lua
@@ -5,18 +5,18 @@ require 'Definitions/HairOutfitDefinitions'
 
 local cat = {};
 cat.outfit = "SharkArmyCamoGreenMale";
-cat.haircut = "Short:100";
+cat.haircut = "Short:50;CrewCut:20;Bald:10";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
 local cat = {};
 cat.outfit = "SharkArmyCamoGreenFemale";
-cat.haircut = "Fresh:30;Bun:20;Kate:20;Ponytail:30";
+cat.haircut = "Bun:20;Kate:20;Ponytail:30";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
 local cat = {};
 cat.outfit = "SharkArmyCamoDesertMale";
-cat.haircut = "Short:50;CrewCut:20;Fresh:20;Bald:10";
+cat.haircut = "Short:50;CrewCut:20;Bald:10";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
@@ -27,13 +27,13 @@ table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
 local cat = {};
 cat.outfit = "SharkArmyInstructor";
-cat.haircut = "Short:50;CrewCut:20;Fresh:30";
+cat.haircut = "Short:50;CrewCut:20;";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
 local cat = {};
 cat.outfit = "SharkArmyCamoUrbanMale";
-cat.haircut = "Fresh:100";
+cat.haircut = "Short:50;CrewCut:20;Bald:10";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
@@ -44,20 +44,6 @@ table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
 
 local cat = {};
 cat.outfit = "SharkArmyCamoUrbanGeared";
-cat.haircut = "Short:50;CrewCut:20;Fresh:20;Bald:10";
+cat.haircut = "Short:50;CrewCut:20;Bald:10";
 cat.beard = "None:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
--- Expanded Helicopter Events --
-
-local cat = {};
-cat.outfit = "1Soldier";
-cat.haircut = "Fresh:20;Bun:20;Kate:20;Ponytail:20";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
-local cat = {};
-cat.outfit = "1Soldier";
-cat.haircut = "Short:30;CrewCut:50;Fresh:20";
-cat.beard = "None:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
-
