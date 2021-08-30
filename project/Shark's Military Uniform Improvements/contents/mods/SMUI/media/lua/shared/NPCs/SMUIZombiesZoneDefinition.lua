b74270ffcb46770b5295fa2a135e81b20828b24a
AuthenticPeach <51739720+AuthenticPeach@users.noreply.github.com>
2021-08-29 21:34:41 -0700

    Test Commit
    This is a test. Hello World.

--- project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/NPCs/SMUIZombiesZoneDefinition.lua
+++ project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/shared/NPCs/SMUIZombiesZoneDefinition.lua
@@ -9,23 +9,59 @@ SMUIZombiesZoneDefinition.Army = {
 		mandatory="true",
 		gender="male",
 	},
-	ArmyInstructor = {
-		name="ArmyInstructor",
-		chance=30,
+	SharkArmyInstructorM = {
+		name="SharkArmyInstructor",
+		toSpawn=1,
+		mandatory="true",
 		gender="male",
 	},
+	GhillieM = {
+		name="Ghillie",
+		toSpawn=1,
+		mandatory="true",
+		gender="male",
+	},		
+	ArmyInstructor = {
+		name="ArmyInstructor",
+		chance=15,
+		gender="male",
+	},
+	SharkArmyInstructor = {
+		name="SharkArmyInstructor",
+		chance=15,
+		gender="male",
+	},	
 	ArmyCamoDesert = {
 		name="ArmyCamoDesert",
-		chance=10,
+		chance=5,
 	},
+	SharkArmyCamoDesertMale = {
+		name="SharkArmyCamoDesertMale",
+		chance=3,
+		gender="male",
+	},
+	SharkArmyCamoDesertFemale = {
+		name="SharkArmyCamoDesertFemale",
+		chance=3,
+		gender="male",
+	},	
 	ArmyCamoGreen = {
 		name="ArmyCamoGreen",
-		chance=90,
+		chance=30,
 	},
+	SharkArmyCamoGreenMale = {
+		name="SharkArmyCamoGreenMale",
+		chance=22,
+	},
+	SharkArmyCamoGreenFemale = {
+		name="SharkArmyCamoGreenFemale",
+		chance=22,
+	},
+	
 }
 
 SMUIZombiesZoneDefinition.SecretBase = {
-	1SoldierMOPP = {
+	ArmyGeneral = {
 		name="ArmyGeneral",
 		chance=1,
 		gender="male",
@@ -65,5 +101,6 @@ SMUIZombiesZoneDefinition.SecretBase = {
 -- total chance can be over 100% we don't care as we'll roll on the totalChance and not a 100 (unlike the specific outfits on top of this)
 SMUIZombiesZoneDefinition.Default = ZombiesZoneDefinition.Default or {};
 
-table.insert(SMUIZombiesZoneDefinition.Default,{name = "ArmyCamoGreen", chance=0.10});
-table.insert(SMUIZombiesZoneDefinition.Default,{name = "1SoldierMOPP", chance=0.10});
+table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyCamoGreenMale", chance=0.04});
+table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyCamoGreenFemale", chance=0.03});
+table.insert(SMUIZombiesZoneDefinition.Default,{name = "SharkArmyInstructor", chance=0.03});
