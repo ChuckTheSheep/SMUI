250cd3e1c9343f5336a2a5469c64f45c33a2fdd9
AuthenticPeach <51739720+AuthenticPeach@users.noreply.github.com>
2021-08-30 01:54:08 -0700

    Update Distributions_SMUI.lua
    Adds Functional Distributions for all items.

--- project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUI.lua
+++ project/Shark's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUI.lua
@@ -1,14 +1,56 @@
-require "Items/SuburbsDistributions"
-require "Items/ItemPicker"
+require 'Items/ProceduralDistributions'
 
-    table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, "Base.Hat_CavalryHat");
-    table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, 5);
-    table.insert(SuburbsDistributions["gunstore"]["locker"].items, "Base.Hat_CavalryHat");
-    table.insert(SuburbsDistributions["gunstore"]["locker"].items, 1.3);
+SMUI = SMUI or {};
 
-    table.insert(SuburbsDistributions["armysurplus"]["metal_shelves"].items, "Base.Hat_CavalryHat");
-    table.insert(SuburbsDistributions["armysurplus"]["metal_shelves"].items, 3);
-    table.insert(SuburbsDistributions["armystorage"]["metal_shelves"].items, "Base.Hat_CavalryHat");
-    table.insert(SuburbsDistributions["armystorage"]["metal_shelves"].items, 3);
+SMUI.tab_addHatsProcedural_items  = function(x,count)
+  ProceduralDistributions = ProceduralDistributions or {};
+  ProceduralDistributions.list = ProceduralDistributions.list or {};
+  ProceduralDistributions.list[x] = ProceduralDistributions.list[x] or {};
+  ProceduralDistributions.list[x].items = ProceduralDistributions.list[x].items or {};
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Hat_Army");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Hat_BeretSpecial");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Hat_CavalryHat");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Hat_PatrolCap");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Hat_PatrolCapDesert");
+  table.insert(ProceduralDistributions.list[x].items, count);
+end
+
+SMUI.tab_addClothesProcedural_items  = function(x,count)
+  ProceduralDistributions = ProceduralDistributions or {};
+  ProceduralDistributions.list = ProceduralDistributions.list or {};
+  ProceduralDistributions.list[x] = ProceduralDistributions.list[x] or {};
+  ProceduralDistributions.list[x].items = ProceduralDistributions.list[x].items or {};
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Jacket_ArmyCamoDesert");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Jacket_ArmyCamoGreen");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Jacket_ArmyCamoUrban");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Trousers_CamoDesert");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Trousers_CamoGreen");
+  table.insert(ProceduralDistributions.list[x].items, count);
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Trousers_CamoUrban");
+  table.insert(ProceduralDistributions.list[x].items, count);  
+end
+SMUI.tab_addShoesProcedural_items  = function(x,count)
+  ProceduralDistributions = ProceduralDistributions or {};
+  ProceduralDistributions.list = ProceduralDistributions.list or {};
+  ProceduralDistributions.list[x] = ProceduralDistributions.list[x] or {};
+  ProceduralDistributions.list[x].items = ProceduralDistributions.list[x].items or {};
+  table.insert(ProceduralDistributions.list[x].items,"SMUIClothing.Shoes_HazmatBoots");
+  table.insert(ProceduralDistributions.list[x].items, count);  
+end
+
+SMUI.tab_addHatsProcedural_items("LockerArmyBedroom",0.8);
+SMUI.tab_addClothesProcedural_items("LockerArmyBedroom",0.8);
+SMUI.tab_addClothesProcedural_items("CampingStoreClothes",0.8);
+SMUI.tab_addClothesProcedural_items("CrateCamping",0.8);
+SMUI.tab_addShoesProcedural_items("CampingStoreLegwear",1.0);
+SMUI.tab_addShoesProcedural_items("CrateCamping",1.0);
 
 
