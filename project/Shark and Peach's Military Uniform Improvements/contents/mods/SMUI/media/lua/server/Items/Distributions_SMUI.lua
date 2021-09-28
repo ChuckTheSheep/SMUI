f92a9f5bd540042ac280a8db7c9764026536f84e
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-09-28 03:24:17 -0400

    Update Distributions_SMUI.lua

--- project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUI.lua
+++ project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUI.lua
@@ -5,6 +5,11 @@ table.insert(ProceduralDistributions["list"]["ArmyStorageOutfit"].items, 3);
 table.insert(ProceduralDistributions["list"]["ArmySurplusOutfit"].items, "SMUIClothing.MilitaryWebbing");
 table.insert(ProceduralDistributions["list"]["ArmySurplusOutfit"].items, 3);
 
+table.insert(ProceduralDistributions["list"]["ArmyStorageOutfit"].items, "SMUIClothing.Hat_M17");
+table.insert(ProceduralDistributions["list"]["ArmyStorageOutfit"].items, 3);
+table.insert(ProceduralDistributions["list"]["ArmySurplusOutfit"].items, "SMUIClothing.Hat_M17");
+table.insert(ProceduralDistributions["list"]["ArmySurplusOutfit"].items, 3);
+
 table.insert(ProceduralDistributions["list"]["CampingStoreClothes"].items, "SMUIClothing.MilitaryWebbing");
 table.insert(ProceduralDistributions["list"]["CampingStoreClothes"].items, 1);
 table.insert(ProceduralDistributions["list"]["CampingStoreClothes"].items, "SMUIClothing.MilitaryWebbing");
