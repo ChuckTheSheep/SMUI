0df693f049927aa7ce174745f11483c7b970589a
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-11-19 01:28:48 -0500

    Update Distributions_SMUIClothing.lua

--- project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUIClothing.lua
+++ project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/server/Items/Distributions_SMUIClothing.lua
@@ -25,15 +25,28 @@ table.insert(ProceduralDistributions.list.CrateCamping.items, 1);
 table.insert(ProceduralDistributions.list.CrateCamping.items, "SMUIClothing.MilitaryWebbing");
 table.insert(ProceduralDistributions.list.CrateCamping.items, 1);
 
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "SMUIClothing.Hat_PatrolCap");
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 3);
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, "SMUIClothing.Hat_PatrolCap");
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, 3);
+
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "SMUIClothing.Hat_PatrolCapDesert");
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 3);
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, "SMUIClothing.Hat_PatrolCapDesert");
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, 3);
+
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "SMUIClothing.Hat_PatrolCapUrban");
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 3);
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, "SMUIClothing.Hat_PatrolCapUrban");
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, 3);
+
 table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "SMUIClothing.Hat_BeretSpecial");
-table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 2);
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 1);
 table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, "SMUIClothing.Hat_BeretSpecial");
-table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, 2);
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, 1);
 
 table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "SMUIClothing.Vest_MilitaryTacticalVest");
 table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.5);
-table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, "SMUIClothing.Vest_MilitaryTacticalVest");
-table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, 0.5);
 
 table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "SMUIClothing.Hat_CavalryHat");
 table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 2);
