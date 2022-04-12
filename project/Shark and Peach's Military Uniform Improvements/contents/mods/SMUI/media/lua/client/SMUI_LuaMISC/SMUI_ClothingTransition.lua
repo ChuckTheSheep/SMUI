c16a9cb63f827cc6185626f5cf149db0d696555b
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-04-12 03:31:25 -0400

    Working in Dislaik's stuff

--- project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/client/SMUI_LuaMISC/SMUI_ClothingTransition.lua
+++ project/Shark and Peach's Military Uniform Improvements/contents/mods/SMUI/media/lua/client/SMUI_LuaMISC/SMUI_ClothingTransition.lua
@@ -1,4 +1,4 @@
-local Functions = require "Dislaik'sClothingPack/Functions"
+local Functions = require "SMUI_ClothingTransition/DislaikFunctions"
 
 ISInventoryMenuElements = ISInventoryMenuElements or {};
 
@@ -23,8 +23,18 @@ end
 
 local onClothingUpdated = function(source)
 
+    Functions.replaceVisualClothing(source, "Dislaik.LowerLegs_KneePads", "Dislaik.LowerLegs_KneePadsALTER",
+    { "Pants" },
+    {   
+        "Base.Trousers_WhiteTINT", "Base.Trousers", "Base.Guillie_Trousers", "Base.Trousers_Padded",
+        "Base.Trousers_PoliceGrey", "Base.Trousers_Fireman", "Base.Trousers_PrisonGuard", "Base.Trousers_Chef",
+        "Base.Trousers_Ranger", "Base.Trousers_Scrubs", "Base.Trousers_Police", "Base.Trousers_Suit",
+        "Base.Trousers_SuitWhite", "Base.Trousers_JeanBaggy", "Base.Trousers_ArmyService", "Base.Trousers_CamoGreen",
+        "Base.Trousers_CamoDesert", "Base.Trousers_CamoUrban", "Base.Trousers_Black", "Base.TrousersMesh_DenimLight",
+        "Base.Trousers_NavyBlue", "Base.Trousers_Denim", "Base.Shorts_CamoUrbanLong", "Dislaik.Trousers_SWAT"
+    })
 
-    Functions.replaceVisualClothing(source, "SMUIClothing.JungleCombatBoots", "Dislaik.LowerArms_ElbowPadsALTER",
+    Functions.replaceVisualClothing(source, "Dislaik.LowerArms_ElbowPads", "Dislaik.LowerArms_ElbowPadsALTER",
     { "SweaterHat", "Sweater", "Jacket", "BathRobe", "Dress", "FullSuitHead", "FullSuit" },
     {   
         "Base.Jacket_CoatArmy", "Base.JacketLong_Doctor", "Base.Jacket_NavyBlue", "Base.Jacket_WhiteTINT",
@@ -40,7 +50,7 @@ local onClothingUpdated = function(source)
         "Base.Boilersuit_Flying", "Base.Boilersuit_Prisoner", "Base.Boilersuit_PrisonerKhaki", "Dislaik.Jacket_SWAT"
     })
 
-    Functions.replaceVisualClothing(source, "Dislaik.Trousers_SWAT", "Dislaik.Trousers_SWATALTER",
+    Functions.replaceVisualClothing(source, "SMUIClothing.Trousers_ERDL", "SMUIClothing.Trousers_ERDLTucked",
     { "Shoes" },
     {   
         "Base.Shoes_BlackBoots", "Base.Shoes_Wellies", "Base.Shoes_RidingBoots", "Base.Shoes_ArmyBoots",
